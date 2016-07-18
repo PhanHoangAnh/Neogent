fe44516e4fb80ab67ac7da44874eea677151c78e
PhanHoangAnh <hoanganh17b@gmail.com>
2016-07-18 23:24:54 +0700

    fix sortable for CreateProduct subsystem

--- SystemApps/CreateProduct/public/js/createProduct/CreateProduct.js
+++ SystemApps/CreateProduct/public/js/createProduct/CreateProduct.js
@@ -1,3 +1,36 @@
+// $(".connectedSortable").sortable({
+//     // connectWith: $(document.getElementById("productAttributes")),
+//     // remove: function(e, ui) {
+
+//     // },
+//     // receive: function(e, ui) {
+//     //     // ui.sender.sortable("cancel");
+//     // },
+//     // placeholder: "ui-sortable-placeholder",
+//     // out: function(event, ui) {
+
+//     // }
+// });
+
+$(function() {
+    var productAttributes = document.querySelector("#productAttributes");
+    $(".connectedSortable").sortable({
+        connectWith: $(productAttributes),
+        cursor: "move",
+        remove: function(e, ui) {
+
+        },
+        receive: function(e, ui) {
+            // ui.sender.sortable("cancel");
+        },
+        placeholder: "ui-sortable-placeholder",
+        out: function(event, ui) {
+
+        }
+
+    });
+})
+
 function requestOptionSets() {
     // Get Attributes List from server
     var currentUrl = window.location.href + "getOptionSets"
@@ -19,6 +52,7 @@ function requestOptionSets() {
     // end of Get Attributes List from server
 }
 var newId = 0;
+
 function createOptionSetPanel(optSet) {
     var _root = document.getElementById("LoadOptionSets");
     while (_root.firstChild) {
