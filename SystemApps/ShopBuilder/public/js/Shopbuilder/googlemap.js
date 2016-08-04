487e19d76a6741d653d9dfd64013108197eda234
PhanHoangAnh <hoanganh17b@gmail.com>
2016-08-04 18:18:01 +0700

    add google map to ShopBuilder

---
+++ SystemApps/ShopBuilder/public/js/Shopbuilder/googlemap.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {
+    var geocoder = new google.maps.Geocoder();
+
+    function geocodePosition(pos) {
+        geocoder.geocode({
+            latLng: pos
+        }, function(responses) {
+            if (responses && responses.length > 0) {
+                updateMarkerAddress(responses[0].formatted_address);
+            } else {
+                updateMarkerAddress('Cannot determine address at this location.');
+            }
+        });
+    }
+
+    function updateMarkerStatus(str) {
+        document.getElementById('markerStatus').innerHTML = str;
+    }
+
+    function updateMarkerPosition(latLng) {
+        document.getElementById('info').innerHTML = [
+            latLng.lat(),
+            latLng.lng()
+        ].join(', ');
+    }
+
+    function updateMarkerAddress(str) {
+        document.getElementById('address').innerHTML = str;
+        document.getElementById('textAddress').value = str;
+    }
+
+    function initialize() {
+        // var latLng = new google.maps.LatLng(-34.397, 150.644);
+        var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+            zoom: 15,
+            center: latLng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        var marker = new google.maps.Marker({
+            position: latLng,
+            title: 'Your current position',
+            map: map,
+            draggable: true
+        });
+
+        // Update current position info.
+        updateMarkerPosition(latLng);
+        geocodePosition(latLng);
+
+        // Add dragging event listeners.
+        google.maps.event.addListener(marker, 'dragstart', function() {
+            updateMarkerAddress('Dragging...');
+        });
+
+        google.maps.event.addListener(marker, 'drag', function() {
+            updateMarkerStatus('Dragging...');
+            updateMarkerPosition(marker.getPosition());
+        });
+
+        google.maps.event.addListener(marker, 'dragend', function() {
+            updateMarkerStatus('Drag ended');
+            geocodePosition(marker.getPosition());
+        });
+    }
+
+    var latLng;
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            latLng = new google.maps.LatLng(pos.lat, pos.lng);
+            // infoWindow.setPosition(pos);
+            // infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+        zoom: 15,
+        center: latLng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    var marker = new google.maps.Marker({
+        position: latLng,
+        title: 'Vị trí hiện tại của bạn',
+        map: map,
+        draggable: true
+    });;
+    // Onload handler to fire off the app.
+    google.maps.event.addDomListener(window, 'load', initialize);
+    // Onload handler to fire off the app.
+    google.maps.event.addDomListener(window, 'load', initialize);
+    google.maps.event.addDomListener(window, "resize", function() {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
+
+})
