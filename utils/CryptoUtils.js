a28dcda58d381c1821942336bef51bb93bc519a0
PhanHoangAnh <hoanganh17b@gmail.com>
2016-10-13 14:08:22 +0700

    uptodate

--- utils/CryptoUtils.js
+++ utils/CryptoUtils.js
@@ -28,7 +28,7 @@ function decryptRequest(req, res, next) {
     }
     try {
         var DecryptionResult = cryptico.decrypt(encrypt_Request, RSAKey);
-        console.log("DecryptionResult: ", DecryptionResult)
+        console.log("DecryptionResult: at /media/chipl/Storage/NodeJSRepository/Neogent/utils/CryptoUtils.js: line 31", DecryptionResult)
         var DecryptRSA = JSON.parse(DecryptionResult.plaintext);
         var aes_key = DecryptRSA.key;
         delete DecryptRSA.key;
